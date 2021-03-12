16ddf25bcdf2cbe9b741c7f60ce1a87e24b9e62f
Jack Ceballos <jackrceballos@gmail.com>
2021-03-11 20:59:28 -0500

    Working on the components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
-import Button from '@material-ui/core/Button';
 import "./App.css";
 import ScheduleCard from "./components/ScheduleCard";
 
 class App extends Component {
   render() {
     return (
-      <ScheduleCard>
-        <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-      </ScheduleCard>
+      <ScheduleCard></ScheduleCard>
     );
   }
 }
--- client/src/components/ScheduleCard/index.js
+++ client/src/components/ScheduleCard/index.js
@@ -1,22 +1,14 @@
 import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+
 
 function ScheduleCard(props) {
     return(
-        <div className="card">
-            <div className="img-container">
-                <img alt={props} src="#"/>
-            </div>
-            <div className="info-container">
-                <ul>
-                    <li>
-                        <strong>Name:</strong>
-                    </li>
-                    <li>
-                        <strong>Schedule:</strong>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <Card>
+            <CardContent>
+                <Typography variant="h2">Hello World</Typography>
+            </CardContent>
+        </Card>
     );
 }
 
