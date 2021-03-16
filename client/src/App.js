b68c7808afb027751cd9b681285dc0dfa9063219
Jack Ceballos <jackrceballos@gmail.com>
2021-03-16 18:38:11 -0400

    Finished the two page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,35 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Button, Container, Paper, Typography } from "@material-ui/core";
-import DoctorCard from "./components/DoctorCard";
+//import { Button, Container, Paper, Typography } from "@material-ui/core";
+//import DoctorCard from "./components/DoctorCard";
+import Appointment from "./pages/Appointment";
+import Schedule from "./pages/Schedule"
 
 class App extends Component {
   render() {
-    const styles = {
-      header: {
-        textAlign: "center",
-        color: "#27251F",
-        backgroundColor: "#8A8D8F",
-        borderColor: "#FA4616",
-        borderStyle: "solid",
-        padding: "5px"
-      }
-    }
+    // const styles = {
+    //   header: {
+    //     textAlign: "center",
+    //     color: "#27251F",
+    //     backgroundColor: "#8A8D8F",
+    //     borderColor: "#FA4616",
+    //     borderStyle: "solid",
+    //     padding: "5px"
+    //   }
+    // }
     return (
-      
-      <Container>
-        <Paper style={styles.header}>
-          <Typography variant="h1">Doctor Scheduler</Typography>
-          <Button variant="contained">Schedule</Button>
-        </Paper>
-        <DoctorCard>
-        </DoctorCard>
-      </Container>
+      <Router>
+
+        <Switch>
+          <Route exact path={["/", "/appointments"]}>
+            <Appointment></Appointment>
+          </Route>
+          <Route exact path="/create-appt">
+            <Schedule></Schedule>
+          </Route>
+        </Switch>
+
+      </Router>
     );
   }
 }
--- client/src/pages/Appointment.js
+++ client/src/pages/Appointment.js
@@ -1,10 +1,32 @@
 import React from "react";
+import DoctorCard from "../components/DoctorCard";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Container, Paper, Typography } from "@material-ui/core";
 
-<Container>
-    <Paper style={styles.header}>
-        <Typography variant="h1">Doctor Scheduler</Typography>
-        <Button variant="contained">Schedule</Button>
-    </Paper>
-    <DoctorCard>
-    </DoctorCard>
-</Container>
+const useStyles = makeStyles({
+    header: {
+        textAlign: "center",
+        color: "#27251F",
+        backgroundColor: "#8A8D8F",
+        borderColor: "#FA4616",
+        borderStyle: "solid",
+        padding: "5px"
+      }
+})
+
+function Appointment() {
+    const classes = useStyles();
+    return (
+
+        <Container>
+            <Paper className={classes.header}>
+                <Typography variant="h1">Doctor Scheduler</Typography>
+                <Button variant="contained">Schedule</Button>
+            </Paper>
+            <DoctorCard>
+            </DoctorCard>
+        </Container>
+    )
+}
+
+export default Appointment;
