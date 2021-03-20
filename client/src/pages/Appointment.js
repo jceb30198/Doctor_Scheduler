f189e8222c994da027742a1c96170928d61d7af4
Jack Ceballos <jackrceballos@gmail.com>
2021-03-19 23:41:27 -0400

    New styles

--- client/src/pages/Appointment.js
+++ client/src/pages/Appointment.js
@@ -7,13 +7,13 @@ import API from "../utils/API";
 const useStyles = makeStyles({
     jumbo: {
         textAlign: "center",
-        backgroundColor: "#8A8D8F",
-        borderColor: "#FA4616",
+        backgroundColor: "#3aba8e",
+        borderColor: "#7eb6af",
         borderStyle: "solid",
         padding: "5px"
     },
     task: {
-        backgroundColor: "#009CA6",
+        backgroundColor: "#3aba8e",
         alignContent: "center",
         marginTop: 10
     },
@@ -23,13 +23,12 @@ const useStyles = makeStyles({
     },
     card: {
         padding: 10,
-        backgroundColor: "#8A8D8F",
-        borderColor: "#FA4616",
+        borderColor: "#7eb6af",
         borderStyle: "solid"
     },
     header: {
-        borderColor: "#8A8D8F",
-        borderBottomColor: "#FA4616",
+        borderColor: "white",
+        borderBottomColor: "#d32b27",
         textAlign: "center",
         borderStyle: "solid",
         borderWidth: "3px"
@@ -64,45 +63,47 @@ function Appointment() {
 
     // console.log(doctors.length); // Testing length
     return (
+        <Grid>
 
-        <Container>
             <Paper className={classes.jumbo}>
                 <Typography variant="h1">Doctor Scheduler</Typography>
                 <Link to={"/schedule"}>
-                    <Button variant="contained">Schedule</Button>
+                    <Button variant="contained">Schedule Appointment</Button>
                 </Link>
             </Paper>
-            <Grid container direction="row">
-                {doctors.map(doctor => {
-                    // console.log(doctor); // Testing object
-                    return (
-                        <Grid container item xs={6} sm={4} className={classes.root}>
-                            <Paper className={classes.card}>
-                                <Grid container direction="row">
-                                    <Grid item xs={12} className={classes.header}>
-                                        <Typography variant="h4">Dr. {doctor.name}
-                                        </Typography>
-                                    </Grid>
-                                    <List className={classes.task}>
+            <Container fluid>
+                <Grid container direction="row">
+                    {doctors.map(doctor => {
+                        // console.log(doctor); // Testing object
+                        return (
+                            <Grid container item xs={6} sm={4} className={classes.root}>
+                                <Paper className={classes.card}>
+                                    <Grid container direction="row">
+                                        <Grid item xs={12} className={classes.header}>
+                                            <Typography variant="h4">Dr. {doctor.name}
+                                            </Typography>
+                                        </Grid>
+                                        <List className={classes.task}>
 
-                                    {doctor.task.map(task => {
-                                         console.log(task); // Testing object
-                                        return (
-                                                <ListItem key={doctor._id}>
-                                                    <ListItemText primary={`${task.date.month}/${task.date.day}/${task.date.year}`} secondary={`${task.time.start} - ${task.time.end}`} />
-                                                    <ListSubheader>ID:{task._id}</ListSubheader>
-                                                    <Button variant="contained" onClick={() => remove(doctor._id)}>X</Button>
-                                                </ListItem>
-                                        )
-                                    })}
-                                    </List>
-                                </Grid>
-                            </Paper>
-                        </Grid>
-                    )
-                })}
-            </Grid>
-        </Container>
+                                            {doctor.task.map(task => {
+                                                console.log(task); // Testing object
+                                                return (
+                                                    <ListItem key={doctor._id}>
+                                                        <ListItemText primary={`${task.date.month}/${task.date.day}/${task.date.year}`} secondary={`${task.time.start} - ${task.time.end}`} />
+                                                        <ListSubheader>ID:{task._id}</ListSubheader>
+                                                        <Button variant="contained" onClick={() => remove(doctor._id)}>X</Button>
+                                                    </ListItem>
+                                                )
+                                            })}
+                                        </List>
+                                    </Grid>
+                                </Paper>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            </Container>
+        </Grid>
     )
 }
 
